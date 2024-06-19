e1172e16d7b81f77c9ea3d395bd34b5050f35bf3
Taimoor <taimour448@gmail.com>
2024-06-19 22:28:45 +0500

    use axios

--- datafetch/vite-project/src/App.jsx
+++ datafetch/vite-project/src/App.jsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from "react";
-
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import axios from 'axios'
 
 function App() {
 const [photos , setPhotos] = useState([])
+const [loading,setLoading] = useState(false)
 
 useEffect(()=>{
 test()  
 
 },[])
+
+
+
   const test = async ()=>{
-    try{
-       const response = await fetch('https://fakestoreapi.com/products')
-          let data = await response.json();
-          console.log(data);
-          setPhotos(data)
-          
-      }
-    catch(err){
-console.log(err);
+    
+    try {
+      setLoading(true)
+      const response = await axios.get('https://fakestoreapi.com/products')
+      const data = response.data
+      setPhotos(data);
+    } catch (error) {
+      console.log(error);
+    }finally{
+       setLoading(false)
     }
 
-    
-    
-    
 
   }
   
@@ -33,12 +36,16 @@ console.log(err);
       <h1>Taimoor</h1>
     
       <button onClick={test} >Test</button>
-<div style={{width:'80%',display:"flex",alignItems:"center", gap:40 , flexWrap:'wrap' , justifyContent:"center",margin:'0 auto'}}>
+      {
+        loading && 
+        <h1><AiOutlineLoading3Quarters fa-spin /></h1>
+      }
+<div style={{width:'80%',display:"flex",alignItems:"center", gap:40 , flexWrap:'wrap' , justifyContent:"center",margin:'0 auto',textAlign:'center'}}>
         {
 
         photos.map((item , index)=>(
             <div key={index} style={{
-              width:'calc(25% - 40px)'
+              width:'calc(25% - 40px)',border:'1px solid #ddd'
               }}>
 
               <img src={item.image} alt="" width='120px'/>
